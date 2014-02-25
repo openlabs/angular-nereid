4227074cf0b4ea1c8f1160e1216899de11469d39
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-02-25 09:49:31 +0530

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'nereidNereidAuth', function($scope, nereidNereidAuth) {
+	//TODO - put any directive code here
+}]);
